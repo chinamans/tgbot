f5840c4e4f7e9689a6b579727ea53aa87d0be136
XiaoZe <32561574+chinamans@users.noreply.github.com>
2025-07-26 14:12:10 +0800

    增加三倍投的策略

--- libs/ydx_betmodel.py
+++ libs/ydx_betmodel.py
@@ -79,7 +79,11 @@ class BetModel(ABC):
         
     def get_bet_bonus(self, start_bonus, bet_count):
         """计算下注金额（基于连败次数）"""
-        return start_bonus * (2.5 ** (bet_count + 1) - 1)
+        return start_bonus * (2 ** (bet_count + 1) - 1)
+
+    def get_bet_trible(self, start_bonus, bet_count):
+        """计算下注金额（基于连败次数三倍投）"""
+        return start_bonus * (3 ** bet_count)
 
 class A(BetModel):
     """固定0的智能预测策略"""
@@ -121,7 +125,7 @@ class A(BetModel):
         self.guess_dx = 0
         return self.guess_dx
 
-    def get_bet_count(self, data: list[int], start_count=0, stop_count=0):
+    def get_bet_trible(self, data: list[int], start_count=0, stop_count=0):
         bet_count = self.fail_count - start_count
         if 0 <= bet_count < stop_count:
             return bet_count
@@ -157,7 +161,7 @@ class B(BetModel):
         self.guess_dx = 1
         return self.guess_dx
 
-    def get_bet_count(self, data: list[int], start_count=0, stop_count=0):
+    def get_bet_trible(self, data: list[int], start_count=0, stop_count=0):
         bet_count = self.fail_count - start_count
         if 0 <= bet_count < stop_count:
             return bet_count
